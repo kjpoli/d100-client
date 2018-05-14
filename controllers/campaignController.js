1603ca2a8b9d3f1ff46a9ce59ec1b181eba68f77
Philip Vitale <philipmvitale@gmail.com>
2018-05-14 14:59:34 -0400

    modify campaign routes

--- controllers/campaignController.js
+++ controllers/campaignController.js
@@ -29,6 +29,18 @@ exports.createCampaign = async function (req, res) {
         attributeTemplate: characterAtts
     });
 
+    const character = new Character({
+        user: req.user._id,
+        name: "Poop",
+        icon: "css",
+        stats: [
+            { name: "str", value: 0, primary: true },
+            { name: "dex", value: 99, primary: true }
+        ]
+    });
+
+    newCampaign.characters.push(character);
+
     User.findById(req.user._id, function (err, user) {
         if (err) {
             res.status('500').send('uh oh');
@@ -45,8 +57,6 @@ exports.createCampaign = async function (req, res) {
         if (err) {
             res.status('500').send('Internal Server Error');
         } else {
-            //res.status('200').json(newCampaign);
-            //res.render('campaign', { message: "tits" });
             res.redirect('/campaign');
             //campaign created!
         }
@@ -55,19 +65,38 @@ exports.createCampaign = async function (req, res) {
 
 exports.joinCampaign = async function (req, res) {
     let campaignId = req.params.id;
-    Campaign.findById(campaignId, function (err, campaign) {
-        if (err) {
-            res.render('campaign', { message: `No campaign of ID ${campaignId} found` });
-        } else {
-            res.render('campaign', { message: `yay` });
-            campaign.characters.findById(req.user._id, function (err, character) {
-                if (err) {
-                    res.status('404').send('you need to create a character');
-                    //res.redirect('/campaign/:id/create');
-                } else {
-                    res.status('200').json(character);
+    let userId = req.user._id;
+    res.status('200').send(campaignId);
+/*
+    Campaign.
+        findById(campaignId).
+        populate('dm', '_id').
+        populate('players', '_id').
+        populate('characters').
+        exec(function (err, campaign) {
+            if (err) {
+                res.status('404').send('Campaign Not Found');
+            } else {
+                //if (userId.equals(campaign.dm._id)) {
+                  //  res.status('200').send('DM');
+                //} else {
+                for (id in campaign.players) {
+                    if (userId.equals(id)) {
+                        res.status('200').send('OK');
+                    }
                 }
-            });
-        }
-    });
+                res.status('200').send('NOt OK');
+                
+                    campaign.players.find(function (err, player) {
+                        if (err) {
+                            res.status('200').send('create character');
+                        } else {
+                            res.render('./layouts/gameboard', { layout: false });
+                        }
+                    });
+                    res.render('./layouts/gameboard', { layout: false });
+                //}
+            }
+        });
+        */
 };
