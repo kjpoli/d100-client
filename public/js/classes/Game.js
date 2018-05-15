c1f576acaf250a8eb35980ee6c22c25185ea6091
poli <kevinjosephpoli@gmail.com>
2018-05-14 21:14:21 -0400

    started game script, getting phil up to speed

---
+++ public/js/classes/Game.js
@@ -0,0 +1,25 @@
+//var socket = io('http://localhost:3000');
+/*
+//get campaign from db
+var bg = [];
+for(let player of players){
+    bg.push(new BattleGridItem(player));
+}
+
+if(dm){
+    var fdm = new FrameDM(players);
+    fdm.insert();
+}
+
+if(player){
+    var fp = new FrameProfile(player);
+    fp.insert();
+}
+*/
+/*
+function bGridItemById(id){
+    for(let gi of bg){
+        if(gi._id == bg.id) return gi;
+    } else return 0;
+}
+*/
